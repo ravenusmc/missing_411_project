8baf8694d2bcf1c701601c6dd85405ac1fe0e712
ravenusmc <mcuddy77@gmail.com>
2024-03-28 16:33:26 -0400

    minor change

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,12 +5,11 @@
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
