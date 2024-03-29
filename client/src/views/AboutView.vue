2d24f1328e64a8ac26efb4d636f9f82693a9a22c
ravenusmc <mcuddy77@gmail.com>
2024-03-29 14:25:03 -0400

    working on about page

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,4 +1,9 @@
 <template>
   <div>
+    <!-- Header area -->
+    <div class="about_header">
+      <h1 class="center">About Me</h1>
+    </div>
+    <!-- End of Header Area -->
   </div>
 </template>
