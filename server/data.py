555cf9bab63d06486f9601a5e6e6825da05c630b
ravenusmc <mcuddy77@gmail.com>
2024-04-10 16:59:11 -0400

    minor change

--- server/data.py
+++ server/data.py
@@ -6,3 +6,11 @@ class ExamineCSV():
 
 def __init__(self):
         self.data = pd.read_csv('./data/Missing.csv')
+
+
+# Graphs to make:
+# number of people missing by year - or decade? 
+# States with the most missing people 
+# -most common ages of people missing 
+# -Sex of people missing - allow user to select decades?
+# -Graph of Volume - Easter or Western US
