00a54304e4912e63cdecc15c17c9787ed3cb04ca
ravenusmc <mcuddy77@gmail.com>
2024-04-13 11:29:08 -0400

    working on data

--- server/data.py
+++ server/data.py
@@ -4,12 +4,12 @@ import pandas as pd
 
 class ExamineCSV():
 
-def __init__(self):
+    def __init__(self):
         self.data = pd.read_csv('./data/Missing.csv')
 
-# Get the top 5 states
-def states_with_most_missing_people():
-
+    # Get the top 5 states
+    def states_with_most_missing_people(self):
+        print(self.data.head())
 
 # Graphs to make:
 # number of people missing by year - or decade? 
@@ -17,3 +17,6 @@ def states_with_most_missing_people():
 # -most common ages of people missing 
 # -Sex of people missing - allow user to select decades?
 # -Graph of Volume - Easter or Western US
+
+obj = ExamineCSV()
+obj.states_with_most_missing_people()
