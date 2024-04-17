0165a1ee36ef7ce06d5915a08b50c85674a032a9
ravenusmc <mcuddy77@gmail.com>
2024-04-17 16:13:29 -0400

    minor change

--- server/data.py
+++ server/data.py
@@ -10,21 +10,14 @@ class ExamineCSV():
     # Get the top 5 states
     def states_with_most_missing_people(self):
         top_5_states = self.data['state/province'].value_counts().head(5)
-        top_5_states = pd.DataFrame(list(top_5_states.items()), columns=['State', 'Values'])
-        top_5_states_list = []
-        columns = ['State', 'Count']
-        top_5_states_list.append(columns)
-        count = 0 
-        while count < len(top_5_states):
-            rows = []
-            state = top_5_states.iloc[count,0]
-            missing_count = int(top_5_states.iloc[count,1])
-            rows.append(state)
-            rows.append(missing_count)
-            top_5_states_list.append(rows)
-            count += 1 
+        top_5_states_list = [['State', 'Count']]  # Initialize the list with column headers
+        for state, count in top_5_states.items():
+            top_5_states_list.append([state, count]) 
         return top_5_states_list
     
+    # Gets the number of missing people by decade 
+    def number_of_people_missing_by_decade(self):
+        pass
 
 
 # Graphs to make:
