4e7bd31292664c58dad44c4afd9e910eb5379db4
ravenusmc <mcuddy77@gmail.com>
2024-05-11 13:51:59 -0400

    working on jumbotron area on landing page

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <router-view />
-    <section>
 
-      <div class='main-image'>
-
+    <div class='main-image'>
+      <div class='title-div'>
+        <h1 class='center'>Missing 411 Study</h1>
+        <hr>
+        <h3 class='center'>A Project Examing Missing Person Data</h3>
       </div>
-
-    </section>
+    </div>
   </div>
 </template>
 
@@ -25,9 +26,19 @@ export default {
 <style scoped>
 
 .main-image {
-  height: 75vh;
+  height: 80vh;
   background: url("../assets/images/home.jpg") center/cover no-repeat; /* Combine background properties */
   background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.title-div {
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 15px;
+  color: white;
+  padding: 20px;
 }
 
 
