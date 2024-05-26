343a165da8550dfc9b6a8d143193b43f55843d19
ravenusmc <mcuddy77@gmail.com>
2024-05-26 13:30:51 -0400

    made purpose component responsive

--- client/src/components/home/Purpose.vue
+++ client/src/components/home/Purpose.vue
@@ -3,12 +3,12 @@
     <h3 class="title center">The Purpose</h3>
     <div class="main-div">
       <p>
-        Sometime during Covid or shortly after Covid I watched a documentary on
+        Sometime during Covid, or shortly after Covid, I watched a documentary on
         Amazon. the documentary was about people who went missing in national
         parks. More than that though it argued that there was something more
         going on. As I watched the documentary I wondered if I could get some of
         the data and examine it myself. I love doing data projects and it seemed
-        perfect. Well a lot of time went by, I got busy with work, other
+        perfect. Time went by, I got busy with work, other
         projects etc and I never spent that much time trying to track down data.
         In the last few months I really started to look for some data on missing
         411 and managed to find something on Kaggle. The problem was that the
@@ -36,7 +36,7 @@ export default {
 }
 
 .main-div {
-  margin: 0 100px;
+  margin: 20px 100px;
   display: grid;
   grid-template-columns: 1fr 1fr;
   font-size: 1.5rem;
@@ -45,8 +45,17 @@ export default {
 
 .picture-div {
   height: 550px;
-	margin-left: 10px;
+	margin-left: 50px;
   background-size: cover;
 	background: url("../../assets/images/swing.jpg") center/cover no-repeat; 
+}
+
+@media only screen and (max-width: 769px) {
+
+  .main-div {
+    margin: 10px 30px;
+    grid-template-columns: 1fr;
+  }
+
 }
 </style>
