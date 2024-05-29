4653e70df8878bf5aecb185dd94068e641283aef
ravenusmc <mcuddy77@gmail.com>
2024-05-29 17:04:21 -0400

    working on bottom component on landing page

--- client/src/components/home/Bottom.vue
+++ client/src/components/home/Bottom.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <hr />
-    <div class='main-div'>
-      <div class="button-div">
-        <button>Login</button>
+    <div class="main-div">
+      <div class="link-div">
+        <a><router-link class="link-appearance" to="/">Login</router-link></a>
+        <hr />
+        <a><router-link class="" to="/">Sign Up</router-link></a>
       </div>
     </div>
   </div>
@@ -17,18 +19,27 @@ export default {
 
 <style scoped>
 .main-div {
-	margin: 10%;
-	display: flex;
-	justify-content: center;
-	align-items: center;
+  margin: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.button-div {
+.link-div {
+  padding: 15px;
   border: 2px solid black;
-  border-radius: 50px;
+  border-radius: 50%;
   width: 100px;
+  height: 100px; /* Ensure the height is equal to the width */
+  box-sizing: border-box; /* Include padding and border in the element's total width and height */
 }
 
-button {
+a {
+  text-decoration: none;
+  text-align: center;
 }
+
+/* .link-appearance {
+  color: blue;
+} */
 </style>
