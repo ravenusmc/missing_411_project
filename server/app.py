515531e4aac3d26278a11e0f821c11c4fc52a642
ravenusmc <mcuddy77@gmail.com>
2024-06-18 16:33:20 -0400

    minor changes

--- server/app.py
+++ server/app.py
@@ -13,10 +13,33 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        db = Connection()
+        post_data = request.get_json()
+        hashed = db.encrypt_pass(post_data)
+        user_created = db.insert(post_data, hashed)
+        return jsonify('5')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        db = Connection()
+        post_data = request.get_json()
+        email = post_data['email']
+        password = post_data['password']
+        login_values = {}
+        # Checking to see if the user is in the database
+        login_flag, not_found, password_no_match, user = db.verify_user(
+            email, password)
+        login_values['login_flag'] = login_flag
+        login_values['Not_found'] = not_found
+        login_values['Password_no_match'] = password_no_match
+        login_values['user'] = user
+    return jsonify(login_values)
+
+
 
 
 
