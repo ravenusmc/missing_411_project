82176f605439390afbbc414bd9882d6494962760
ravenusmc <mcuddy77@gmail.com>
2024-07-17 15:34:30 -0400

    setting up vuex for the user sign in

---
+++ client/src/store/modules/common.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+import store from '@/store/index';
+
+Vue.use(Vuex);
+
+const data = {
+	dataReceived: false,
+	mapData: {MI: 1},
+};
